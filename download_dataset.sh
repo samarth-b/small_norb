43f565f15f4801ba4cf6c943c4d6a459d29739dc
Samarth Bharadwaj <samarthbharadwaj@gmail.com>
2019-04-03 15:40:07 +0530

    a shell script to download the data as well
    simply added the links from the small norb dataset

---
+++ download_dataset.sh
@@ -0,0 +1,9 @@
+mkdir small_norb_root
+
+wget --directory-prefix=./small_norb_root https://cs.nyu.edu/~ylclab/data/norb-v1.0-small/smallnorb-5x46789x9x18x6x2x96x96-training-dat.mat.gz
+wget --directory-prefix=./small_norb_root https://cs.nyu.edu/~ylclab/data/norb-v1.0-small/smallnorb-5x46789x9x18x6x2x96x96-training-cat.mat.gz
+wget --directory-prefix=/small_norb_root https://cs.nyu.edu/~ylclab/data/norb-v1.0-small/smallnorb-5x46789x9x18x6x2x96x96-training-info.mat.gz
+
+wget --directory-prefix=./small_norb_root https://cs.nyu.edu/~ylclab/data/norb-v1.0-small/smallnorb-5x46789x9x18x6x2x96x96-testing-dat.mat.gz
+wget --directory-prefix=./small_norb_root https://cs.nyu.edu/~ylclab/data/norb-v1.0-small/smallnorb-5x46789x9x18x6x2x96x96-testing-cat.mat.gz
+wget --directory-prefix=./small_norb_root https://cs.nyu.edu/~ylclab/data/norb-v1.0-small/smallnorb-5x46789x9x18x6x2x96x96-testing-info.mat.gz
